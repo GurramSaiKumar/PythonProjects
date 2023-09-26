2667b984f53133ec299b732e2b0f4bb92664b1cf
SaiKumar <sai@essenvia.com>
2023-09-26 23:53:22 +0530

    Added finding_pallindromes.py

---
+++ impractical_python_projects/finding_pallindromes.py
@@ -0,0 +1,20 @@
+def find_palindromes():
+    list_10 = []
+    with open('utils/dictionary.txt', 'r') as file:
+        # while True:
+        #     content = file.readline()
+        #     if len(list_10) > 10:
+        #         break
+        #     list_10.append(content.strip())
+        for _ in range(10):
+            content = file.readline()
+            if not content:
+                break
+            list_10.append(content.strip())
+    print(list_10)
+
+
+
+
+if __name__ == '__main__':
+    find_palindromes()
